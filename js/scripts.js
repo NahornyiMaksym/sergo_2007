509b6aa3995cf28ecf7f58839a2c0e2d376467e7
Maksym.info <nagornymaxim@gmail.com>
2022-05-04 13:23:15 +0300

    finished footer, ventajas, servisios, comentarios carousel finished but picture not on full width in mob version

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+document.querySelector('.second-button').addEventListener('click', function () {
+
+    document.querySelector('.animated-icon2').classList.toggle('open');
+});
+
+
+$(document).ready(function () {
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        navText: ['', ' '],
+
+        responsive: {
+            0: {
+                items: 1
+            },
+
+            1000: {
+                items: 1
+            }
+        }
+    });
+});
