1cf941820a64b1770594c51d8f07492b6e8b915c
VitLozov <v.lozovskyi@gmail.com>
2022-05-09 17:05:11 +0300

    PHP form updated

--- send-email.php
+++ send-email.php
@@ -26,6 +26,9 @@ $headers = "From: $email_from \r\n";
 
 $headers .= "Reply-To: $visitor_email \r\n";
 
-mail($to,$email_subject,$email_body,$headers);
-
+if(mail($to,$email_subject,$email_body,$headers)) {
+    echo 'Email sent';
+} else {
+    echo 'Error, try again.'
+}
     ?>
